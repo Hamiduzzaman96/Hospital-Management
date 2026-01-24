638fc4e6e3dd4036c10cd8cfd70556d2709d7a76
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-25 00:27:19 +0600

    hospital_doctor_relationship crud created

---
+++ internal/repository/hospital_doctor_relationship_repo.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/Hamiduzzaman96/Hospital-Management.git/internal/domain"
+)
+
+type HospitalDoctorRelationship struct {
+	db *sql.DB
+}
+
+func NewHospitalDoctorRelationship(db *sql.DB) *HospitalDoctorRelationship {
+	return &HospitalDoctorRelationship{db: db}
+}
+
+func (r *HospitalDoctorRelationship) DoctorAssign(h *domain.HospitalDoctorRelationship) error {
+	_, err := r.db.Exec(
+		`INSERT INTO hospital_doctor_rel (hospital_id, doctor_id) VALUES ($1, $2)`,
+		h.HospitalID, h.DoctorID,
+	)
+	return err
+}
+
+func (r *HospitalDoctorRelationship) RemoveDoctor(hospitalID, doctorID int64) error {
+	_, err := r.db.Exec(
+		`DELETE FROM hospital_doctor_rel WHERE hospital_id = $1 AND doctor_id = $2`,
+		hospitalID, doctorID,
+	)
+	return err
+}
