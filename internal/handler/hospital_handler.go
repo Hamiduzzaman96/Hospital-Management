4b1e1aea252d1eae1572b761c4b81eac668acc09
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-25 23:22:01 +0600

    updated hospital handler

--- internal/handler/hospital_handler.go
+++ internal/handler/hospital_handler.go
@@ -3,30 +3,59 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/Hamiduzzaman96/Hospital-Management.git/internal/domain"
+	"github.com/Hamiduzzaman96/Hospital-Management.git/internal/middleware"
 	"github.com/Hamiduzzaman96/Hospital-Management.git/internal/usecase"
 )
 
 type HospitalHandler struct {
-	hu *usecase.HospitalUsecase
+	hh *usecase.HospitalUsecase
 }
 
-func NewHospitalHandler(huc *usecase.HospitalUsecase) *HospitalHandler {
-	return &HospitalHandler{hu: huc}
+func NewHospitalHandler(hc *usecase.HospitalUsecase) *HospitalHandler {
+	return &HospitalHandler{hh: hc}
 }
 
 func (h *HospitalHandler) Create(w http.ResponseWriter, r *http.Request) {
-	var hospitalList domain.Hospital
+	var hospital domain.Hospital
 
-	if err := json.NewDecoder(r.Body).Decode(&hospitalList); err != nil {
-		http.Error(w, "Invalid JSON format", 400)
+	if err := json.NewDecoder(r.Body).Decode(&hospital); err != nil {
+		http.Error(w, err.Error(), 400)
 		return
 	}
 
-	role := domain.User{
-		Role: domain.SuperAdmin,
+	user := r.Context().Value(middleware.UserContextKey).(domain.User)
+
+	if err := h.hh.Create(user, &hospital); err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Hospital created successfully",
+	})
+}
+
+func (h *HospitalHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" {
+		http.Error(w, "Hospital ID is required", 400)
+		return
 	}
 
-	h.hu.Create(role, &hospitalList)
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid hospital ID", http.StatusBadRequest)
+		return
+	}
+
+	hospital, err := h.hh.GetByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(hospital)
 }
