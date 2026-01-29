f7d259ece7ce74d54272a24a91e4b9b0c79fa6cd
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-29 22:11:47 +0600

    doctor repo updated

--- internal/repository/doctor_repo.go
+++ internal/repository/doctor_repo.go
@@ -16,21 +16,21 @@ func NewDoctorRepository(db *sql.DB) *DoctorRepository {
 
 func (r *DoctorRepository) Create(doc *domain.Doctor) error {
 	return r.db.QueryRow(
-		`INSERT INTO doctors (name, email) VALUES ($1, $2) RETURNING id`,
+		`INSERT INTO doctors (name, email) VALUES ($1, $2) RETURNING doc_id`,
 		doc.Name, doc.Email,
 	).Scan(&doc.DocID)
 }
 
 func (r *DoctorRepository) Update(doc *domain.Doctor) error {
 	_, err := r.db.Exec(
-		`UPDATE doctors SET name = $1, email = $2 WHERE id = $3`,
+		`UPDATE doctors SET name = $1, email = $2 WHERE doc_id = $3`,
 		doc.Name, doc.Email, doc.DocID,
 	)
 	return err
 }
 
-func (r *DoctorRepository) Delete(id int64) error {
-	result, err := r.db.Exec(`DELETE FROM WHERE id = $1`, id)
+func (r *DoctorRepository) Delete(doc_id int64) error {
+	result, err := r.db.Exec(`DELETE FROM WHERE doc_id = $1`, doc_id)
 	if err != nil {
 		return err
 	}
@@ -43,11 +43,11 @@ func (r *DoctorRepository) Delete(id int64) error {
 	return nil
 }
 
-func (r *DoctorRepository) GetByID(id int64) (*domain.Doctor, error) {
+func (r *DoctorRepository) GetByID(doc_id int64) (*domain.Doctor, error) {
 	var doc domain.Doctor
 	err := r.db.QueryRow(
-		`SELECT id, name, email FROM doctors WHERE id = $1`,
-		id,
+		`SELECT id, name, email FROM doctors WHERE doc_id = $1`,
+		doc_id,
 	).Scan(&doc.DocID, &doc.Name, &doc.Email)
 	if err != nil {
 		return nil, err
